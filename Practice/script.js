f17042ee55e056c1c64be0c435c6f27b2feac0fa
codeweb123 <jiyonj@hotmail.com>
2021-01-10 16:18:25 -0500

    modal practice complete

--- Practice/script.js
+++ Practice/script.js
@@ -6,7 +6,6 @@ const btnsOpenModal = document.querySelectorAll('.show-modal')
 console.log(btnsOpenModal)
 
 const openModal = function () {
-    console.log('Button clicked')
     modal.classList.remove('hidden')
     overlay.classList.remove('hidden');
 }
@@ -21,3 +20,11 @@ btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal)
 overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function(e) {
+    console.log(e.key) //.key shows what key was pressed
+
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal()
+    }
+})
