e8e350d41985b333e05e1996f80fc31e3a4c497d
codeweb123 <jiyonj@hotmail.com>
2021-01-10 13:39:56 -0500

    update css

--- Practice/script.js
+++ Practice/script.js
@@ -5,14 +5,19 @@ const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 console.log(btnsOpenModal)
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener('click', function() {
+const openModal = function () {
     console.log('Button clicked')
     modal.classList.remove('hidden')
-    overlay.classList.remove('hidden')
-})
+    overlay.classList.remove('hidden');
+}
 
-btnCloseModal.addEventListener('click', function() {
-    modal.classList.add('hidden');
+const closeModal = function () {
+    modal.classList.add('hidden')
     overlay.classList.add('hidden');
-})
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
