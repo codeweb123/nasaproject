a43bdca958745184bba593c0347a74d2d26d312e
codeweb123 <jiyonj@hotmail.com>
2021-02-22 13:28:00 -0500

    create API

---
+++ APIpractice/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const request = fetch('https://restcountries.eu/rest/v2/name/portugal')
+
+const getCountryData = function(country) {
+    fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(function(response)
+{
+    console.log(response)
+    return response.json() //async function(another promise) method that shows us the JSON from the response object.
+})
+    .then(function(data){ //need another .then to resolve the Promise
+    console.log(data)
+    renderCountry(data[0])
+})
+}
+
+getCountryData('portugal')
