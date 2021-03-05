23fb065371f2061d50a0c1269c584d3f2c71fda8
codeweb123 <jiyonj@hotmail.com>
2021-03-04 23:37:09 -0500

    updates

--- APIpractice/script.js
+++ APIpractice/script.js
@@ -3,15 +3,16 @@
 const request = fetch('https://restcountries.eu/rest/v2/name/portugal')
 
 const getCountryData = function(country) {
-    fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(function(response)
-{
-    console.log(response)
-    return response.json() //async function(another promise) method that shows us the JSON from the response object.
-})
-    .then(function(data){ //need another .then to resolve the Promise
-    console.log(data)
-    renderCountry(data[0])
-})
+    fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then((response) => response.json())
+    .then(data => renderCountry(data[0]))
+    //.then(function(response)
+   // console.log(response)
+   // return response.json() //async function(another promise) method that shows us the JSON from the response object.
 }
+//    .then(function(data){ //need another .then to resolve the Promise
+ //   console.log(data)
+  //  renderCountry(data[0])
+
 
 getCountryData('portugal')
