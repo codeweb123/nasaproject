4f554be15d509f509a913b42326231213670b5e4
codeweb123 <jiyonj@hotmail.com>
2021-03-30 16:47:50 -0400

    update

---
+++ JSInterview/script.js
@@ -0,0 +1,9 @@
+//Interview cheat sheet
+
+//1. Clarify what is being asked, inputs and outputs.
+//2. How many coding excersises will be asked during session?
+//3. Speak while you code.
+//4. Write test cases for your code. (before you start) Edge cases( 2 examples)
+
+//func.apply = calling (this, arguments)
+//.call
