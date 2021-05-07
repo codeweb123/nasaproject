5ec652120f264efd1224df520cbd1b1e4fb65d99
codeweb123 <jiyonj@hotmail.com>
2021-05-06 21:56:47 -0400

    initial commit

---
+++ math-game/script.js
@@ -0,0 +1,52 @@
+// Pages
+const gamePage = document.getElementById('game-page');
+const scorePage = document.getElementById('score-page');
+const splashPage = document.getElementById('splash-page');
+const countdownPage = document.getElementById('countdown-page');
+// Splash Page
+const startForm = document.getElementById('start-form');
+const radioContainers = document.querySelectorAll('.radio-container');
+const radioInputs = document.querySelectorAll('input');
+const bestScores = document.querySelectorAll('.best-score-value');
+// Countdown Page
+const countdown = document.querySelector('.countdown');
+// Game Page
+const itemContainer = document.querySelector('.item-container');
+// Score Page
+const finalTimeEl = document.querySelector('.final-time');
+const baseTimeEl = document.querySelector('.base-time');
+const penaltyTimeEl = document.querySelector('.penalty-time');
+const playAgainBtn = document.querySelector('.play-again');
+
+// Equations
+let questionAmount = 0;
+let equationsArray = [];
+let playerGuessArray = [];
+let bestScoreArray = [];
+
+// Game Page
+let firstNumber = 0;
+let secondNumber = 0;
+let equationObject = {};
+const wrongFormat = [];
+
+// Time
+let timer;
+let timePlayed = 0;
+let baseTime = 0;
+let penaltyTime = 0;
+let finalTime = 0;
+let finalTimeDisplay = '0.0';
+
+startForm.addEventListener('click', () => {
+    radioContainers.forEach((radioEl) => {
+        //remove selected label styling
+        radioEl.classList.remove('selected-label');
+        // add back if radio input is checked
+        if (radioEl.children[1].checked) {
+            radioEl.classList.add('selected-label');
+        }
+    });
+});
+
+startForm.addEventListener('click', selectQuestionAmount);
