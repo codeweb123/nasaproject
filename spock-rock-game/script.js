a7bfe4e44ddfaff35b09e05cc89c3ab5ec77faf9
codeweb123 <jiyonj@hotmail.com>
2021-06-29 17:32:05 -0400

    update

--- spock-rock-game/script.js
+++ spock-rock-game/script.js
@@ -1,3 +1,24 @@
+import { startConfetti, stopConfetti, removeConfetti } from './confetti.js';
+
+const playerScoreEl = document.getElementById('playerScore');
+const playerChoiceEl = document.getElementById('playerChoice');
+const computerScoreEl = document.getElementById('computerScore');
+const computerChoiceEl = document.getElementById('computerChoice');
+
+const playerRock = document.getElementById('playerRock');
+const playerPaper = document.getElementById('playerPaper');
+const playerScissors = document.getElementById('playerScissors');
+const playerLizard = document.getElementById('playerLizard');
+const playerSpock = document.getElementById('playerSpock');
+
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper');
+const computerScissors = document.getElementById('computerScissors');
+const computerLizard = document.getElementById('computerLizard');
+const computerSpock = document.getElementById('computerSpock');
+
+const allGameIcons = document.querySelectorAll('.far');
+const resultText = document.getElementById('resultText');
 
 const choices = {
     rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
@@ -6,3 +27,14 @@ const choices = {
     lizards: { name: 'Lizard', defeats: ['paper', 'spock'] },
     spock: { name: 'Spock', defeats: ['scissors', 'rock'] }
 };
+
+let playerScoreNumber = 0;
+let computerScoreNumber = 0;
+let computerChoice = '';
+
+function select(playerChoice) {
+    switch (playerChoice) {
+        case 
+    }
+}
+
