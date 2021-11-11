750fb5909cfc8d023318d2ef00a7b5d92974f50f
codeweb123 <jiyonj@hotmail.com>
2021-11-11 13:50:35 -0600

    .forEach prac

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,7 +1,6 @@
 //Array methods- functions attached to objects.
 //forEach loops over entire array and cannot break out of it.
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]
-
 //Looping over each element in array using For Of
 for (const movement of movements) {
     if (movement > 0) {
@@ -13,6 +12,7 @@ for (const movement of movements) {
 //
 
 //Looping over each element in array using the index
+//
 for (const [i, movement] of movements.entries()) {
     if (movement > 0) {
         console.log(`Movement ${i + 1}: You deposited ${movement}`)
@@ -20,7 +20,6 @@ for (const [i, movement] of movements.entries()) {
         console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`)
     }
 }
-
 //Looping over using forEach uses a CALLBACK function and you pass in the arguments.
 movements.forEach(function(movement) {
     if (movement > 0) {
@@ -29,18 +28,22 @@ movements.forEach(function(movement) {
         console.log(`You withdrew ${Math.abs(movement)}`)
     }
 })
-
 // forEach can be used with index and array as well.
-movements.forEach(function(movement, i, array) {
-    if (movement > 0) {
-        console.log(`Movement ${i + 1}: You deposited ${movement}`)
+movements.forEach(function(mov, i, arr) {
+    if (mov > 0) {
+        console.log(`Movement ${i + 1}: You deposited ${mov}`)
     } else {
         console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`)
     }
 })
+const holidays = ['Christmas', 'Thanksgiving', 'Fourth of July']
+holidays.forEach(function(hol) {
+    if(hol === 'Christmas' || hol === 'Thanksgiving') {
+        console.log(`Holiday: ${hol}`)
+    }
+})
 //Created my Own!!!
 const animals = ['whale', 'horse', 'turtle', 'lizard']
-
 animals.forEach(function(el) {
 	if(el === 'whale' || el === 'horse') {
         console.log(`Hi, I'm a mammal called a ${el}.`)
@@ -61,7 +64,8 @@ children.forEach(function(el, i) {
     }
     }
 )
-//New Map
+//New Map- Array that houses key/value pairs -objects
+//key/value pairs ['uhu', 'JIKO'],
 const currencies = new Map([
     ['USD', 'United States dollar'],
     ['EUR', 'Euro'],
