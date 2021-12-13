6d0ce18d9542fee3fe18eb911d0f18ad31ef4891
codeweb123 <jiyonj@hotmail.com>
2021-12-13 12:24:47 -0600

    add js

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,1 +1,36 @@
-const movieInfo = document.querySelector(".movie-info");
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6a301a2f4dca0198d12599f7211fd3ee&page=1";
+
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=6a301a2f4dca0198d12599f7211fd3ee&query='' ";
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+//Get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  showMovies(data.results);
+}
+
+function showMovies(movies) {}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = " ";
+  } else {
+    window.location.reload();
+  }
+});
