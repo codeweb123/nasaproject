57222688e884dd68228e0e69e098884028ada9a1
codeweb123 <jiyonj@hotmail.com>
2021-12-14 16:02:09 -0600

    initial commit

---
+++ movie-app2/script.js
@@ -0,0 +1,7 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6a301a2f4dca0198d12599f7211fd3ee&page=1";
+
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=6a301a2f4dca0198d12599f7211fd3ee&query='' ";
