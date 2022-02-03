e46c620febba5d685b0255a1d73d7f67fc0e96ea
codeweb123 <jiyonj@hotmail.com>
2022-02-02 19:49:11 -0600

    add css

---
+++ insectgame/script.js
@@ -0,0 +1,5 @@
+const screens = document.querySelectorAll(".screen");
+const choose_insect_btns = document.querySelectorAll(".choose-insect-btn");
+const start_btn = document.getElementById("start-btn");
+const game_container = document.querySelector(".game-container");
+c;
