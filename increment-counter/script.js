013231ef9d8ed96fd8fac7a1bab31f8809691966
codeweb123 <jiyonj@hotmail.com>
2022-02-06 20:23:02 -0600

    update

--- increment-counter/script.js
+++ increment-counter/script.js
@@ -6,6 +6,15 @@ counters.forEach((counter) => {
   const updateCounter = () => {
     const target = +counter.getAttribute("data-target");
     const c = +counter.innerHTML;
+
     const increment = target / 200;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
   };
+  updateCounter();
 });
