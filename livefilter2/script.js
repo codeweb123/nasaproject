0743661464ab79977a9d2f9a2fee0616b4a908d7
codeweb123 <jiyonj@hotmail.com>
2022-02-23 19:30:32 -0600

    add js

---
+++ livefilter2/script.js
@@ -0,0 +1,17 @@
+const result = document.getElementById("result");
+const filter = document.getElementById("filter");
+
+const listItems = [];
+
+getData();
+
+async function getData() {
+  const res = await fetch("https://randomuser.me/api?results=50");
+
+  const data = await res.json();
+
+  result.innerHTML = " ";
+  data.forEach((user) => {
+      const li
+  })
+}
