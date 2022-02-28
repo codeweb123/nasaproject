05bcfe476de2288805b05c945ef794f569ee2f16
codeweb123 <jiyonj@hotmail.com>
2022-02-28 09:23:18 -0600

    add js

--- surveyUi/script.js
+++ surveyUi/script.js
@@ -1,1 +1,12 @@
-const ratings = document.querySelector("rating");
+const ratings = document.querySelectorAll(".rating");
+const sendBtn = document.querySelector("#send");
+const panel = document.querySelector("#panel");
+
+panel.addEventListener("click", (e) => {
+  if (e.target.parentNode.classList.contains("rating")) {
+    removeActive();
+    e.target.parentNode.classList.add("active");
+  }
+});
+
+function removeActive() {}
