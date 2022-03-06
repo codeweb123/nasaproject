df26e225951505168bc628262e4b0e352519d386
codeweb123 <jiyonj@hotmail.com>
2022-03-06 17:00:01 -0600

    update cpmplete

--- rowOfImages/script.js
+++ rowOfImages/script.js
@@ -2,4 +2,16 @@ const container = document.querySelector(".container");
 const unsplashURL = "https://source.unsplash.com/random/";
 const rows = 3;
 
-for(let i = 0; i < )
+for (let i = 0; i < rows * 3; i++) {
+  const img = document.createElement("img");
+  img.src = `${unsplashURL}${getRandomSize()}`;
+  container.appendChild(img);
+}
+
+function getRandomSize() {
+  return `${getRandomNr()}x${getRandomNr()}`;
+}
+
+function getRandomNr() {
+  return Math.floor(Math.random() * 5) + 300;
+}
