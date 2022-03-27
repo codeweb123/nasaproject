c0f2da48f417b56f9ab1716ab510cefc524ae885
codeweb123 <jiyonj@hotmail.com>
2022-03-26 21:35:58 -0500

    update

---
+++ clock/script.js
@@ -0,0 +1,18 @@
+const hourEl = document.querySelector(".hour");
+const hourEl = document.querySelector(".minute");
+const hourEl = document.querySelector(".second");
+const hourEl = document.querySelector(".time");
+const hourEl = document.querySelector(".date");
+const hourEl = document.querySelector(".toggle");
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const months = ["Jan", "Feb", "Mar"];
