8b506872862db29d011c6f5f1d2a78f9193987a2
codeweb123 <jiyonj@hotmail.com>
2022-03-26 21:36:57 -0500

    update

--- clock/script.js
+++ clock/script.js
@@ -15,4 +15,17 @@ const days = [
   "Saturday",
 ];
 
-const months = ["Jan", "Feb", "Mar"];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
