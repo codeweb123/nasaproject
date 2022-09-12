0f09e2bc70d916dab91d59a36d742a34f5b8aa0f
codeweb123 <jiyonj@hotmail.com>
2022-09-12 14:05:03 -0500

    update

--- infinte-scroll/script.js
+++ infinte-scroll/script.js
@@ -1,3 +1,11 @@
 const count = 10;
 const apiKey = "iFGcsJGbAFjC-mkoHNQu2w3PIlxnD5EC_QVbIUostEQ";
 const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&count=${count}`;
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {}
+}
