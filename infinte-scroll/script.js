08c53f89bb78e89ed8d83a597f0a3925f5612cd5
codeweb123 <jiyonj@hotmail.com>
2022-09-14 13:21:46 -0500

    display photo

--- infinte-scroll/script.js
+++ infinte-scroll/script.js
@@ -7,11 +7,27 @@ const count = 10;
 const apiKey = "iFGcsJGbAFjC-mkoHNQu2w3PIlxnD5EC_QVbIUostEQ";
 const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&count=${count}`;
 
+function displayPhotos() {
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+    item.setAttribute("href", photo.links.html);
+    item.setAttribute("target", "_blank");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("alt", photo.alt_description);
+    img.setAttribute("title", photo.alt_description);
+
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photosArray = await response.json();
+    displayPhotos();
   } catch (error) {}
 }
 
