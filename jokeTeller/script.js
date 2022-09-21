0a4f237ccacf4aa2525ffa8746eee76acad26f0b
codeweb123 <jiyonj@hotmail.com>
2022-09-21 10:56:14 -0500

    add child

--- jokeTeller/script.js
+++ jokeTeller/script.js
@@ -5,7 +5,7 @@ function tellMe(joke) {
   const jokeString = joke.trim().replace(/ /g, "%20");
   // VoiceRSS Speech Parameters
   VoiceRSS.speech({
-    key: "YOUR_API_KEY_HERE",
+    key: "b1e0fb006377467dbf2a49d8a4640c5e",
     src: jokeString,
     hl: "en-us",
     r: 0,
@@ -18,12 +18,12 @@ function tellMe(joke) {
 // Get jokes from Joke API
 async function getJokes() {
   const apiUrl =
-    "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+    "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,explicit";
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
     console.log(data);
-    // Assign One or Two Part Joke
+
     if (data.setup) {
       joke = `${data.setup} ... ${data.delivery}`;
     } else {
