361c1b5c9770818f0d47e303fe8105316f0215ce
codeweb123 <jiyonj@hotmail.com>
2022-09-29 11:23:24 -0500

    create bookmark

--- bookmark/script.js
+++ bookmark/script.js
@@ -48,6 +48,13 @@ function buildBookmarks() {
     closeIcon.setAttribute("onclick", `deleteBookmark('${url}')`);
     const linkInfo = document.createElement("div");
     linkInfo.classList.add("name");
+    const favicon = document.createElement("img");
+    favicon.setAttribute(
+      "src",
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute("alt", "Favicon");
+    const link = document.createElement("a");
   });
 }
 
