42d0d82108cea88a0f80dfc8b41ce3428ed3f98a
codeweb123 <jiyonj@hotmail.com>
2022-11-08 16:28:47 -0600

    append btn

--- starwars/app.js
+++ starwars/app.js
@@ -1,7 +1,15 @@
 const url = "https://swapi.co/api/planets";
+const btn = document.createElement("button");
+
+btn.textContent = "press me";
+document.body.appendChild(btn);
 
 function fetchData() {
-  fetch(url).then(function (rep) {
-    console.log(rep);
-  });
+  fetch(url)
+    .then(function (rep) {
+      return rep.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
