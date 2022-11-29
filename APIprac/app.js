caa276554b62d63a7127e7a85361e9605160b7a9
codeweb123 <jiyonj@hotmail.com>
2022-11-28 22:59:19 -0600

    add arrow function

--- APIprac/app.js
+++ APIprac/app.js
@@ -1,9 +1,50 @@
-const url = "http://swapi.dev/api/planets";
 const btn = document.createElement("button");
+btn.textContent = "press me";
+document.body.appendChild(btn);
+btn.addEventListener("click", function () {
+  fetchData("http://swapi.dev/api/planets");
+});
+const output = document.createElement("div");
+document.body.appendChild(output);
+
+function fetchData(url) {
+  fetch(url)
+    .then((rep) => rep.json())
+    .then(function (data) {
+      output.textContent = `${data.count} results found.`;
+      if (data.next) {
+        const btn2 = document.createElement("button");
+        btn2.textContent = "next";
+        output.appendChild(btn2);
+        btn2.addEventListener("click", function () {
+          fetchData(data.next);
+        });
+      }
+      console.log(data);
+    });
+}
+
+/* const btn = document.createElement("button");
 btn.textContent = "Press Me!";
 document.body.appendChild(btn);
 
-const fetchData = () =>
-  fetch(url).then((rep) => rep.json().then((data) => console.log(data)));
+const output = document.createElement("div");
+document.body.appendChild(output);
 
-btn.addEventListener("click", fetchData);
+btn.addEventListener("click", function () {
+  fetchData("http://swapi.dev/api/planets");
+});
+
+const fetchData = (url) =>
+  fetch(url)
+    .then((rep) => rep.json())
+    .then((data) => (output.textContent = `${data.count} results found.`));
+if (data.next) {
+  const btn2 = document.createElement("button");
+  btn2.textContent = "next";
+  output.appendChild(btn2);
+  btn2.addEventListener("click", function () {
+    fetchData(data.next);
+  });
+  console.log(data);
+} */
