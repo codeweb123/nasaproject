649fc375bc6ca761d710ca2fc88e77f6311db0d0
codeweb123 <jiyonj@hotmail.com>
2022-11-28 21:24:38 -0600

    update

--- APIprac/app.js
+++ APIprac/app.js
@@ -1,7 +1,5 @@
 const url = "http://swapi.dev/api/planets";
 
-const fetchData = function() {
-  fetch(url).then((rep) => response.json() 
-    console.log(rep)
-  );
-}
+const fetchData = function () {
+  fetch(url).then((rep) => console.log(rep));
+};
