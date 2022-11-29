863a616fdce8514abb1586d751c62c816fbead59
codeweb123 <jiyonj@hotmail.com>
2022-11-28 21:42:55 -0600

    add eventlistener

--- APIprac/app.js
+++ APIprac/app.js
@@ -1,5 +1,9 @@
 const url = "http://swapi.dev/api/planets";
+const btn = document.createElement("button");
+btn.textContent = "Press Me!";
+document.body.appendChild(btn);
 
-const fetchData = function () {
-  fetch(url).then((rep) => response.json().then((data) => console.log(data)));
-};
+const fetchData = () =>
+  fetch(url).then((rep) => rep.json().then((data) => console.log(data)));
+
+btn.addEventListener("click", fetchData);
