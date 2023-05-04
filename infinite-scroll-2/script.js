26a1d372d5aaea65257a374e3afa7c9acbb18c39
codeweb123 <jiyonj@hotmail.com>
2023-05-04 13:25:22 -0500

    update

--- infinite-scroll-2/script.js
+++ infinite-scroll-2/script.js
@@ -1,3 +1,31 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let photosArray = [];
+
 const count = 10;
-const apiKey = "";
+const apiKey = "w2sZeKJpyMJrmHd46vRmOI9SuONeH9TohEFjFEuzcdQ";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function displayPhotos() {
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+    item.setAttribute("href", photo.links.html);
+    item.setAttribute("target", "_blank");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("alt", photos.alt_description);
+    img.setAttribute("title", photo.alt_description);
+  });
+}
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {}
+}
+
+getPhotos();
