504f4e075de7662ca9216995fa6ca87c549934ca
codeweb123 <jiyonj@hotmail.com>
2023-05-09 13:06:19 -0500

    update

--- infinite-scroll-2/script.js
+++ infinite-scroll-2/script.js
@@ -1,13 +1,26 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
-
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
-const count = 10;
+const count = 30;
 const apiKey = "w2sZeKJpyMJrmHd46vRmOI9SuONeH9TohEFjFEuzcdQ";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function imageLoaded() {
+  console.log("image loaded");
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    console.log("ready =", ready);
+  }
+}
+
 function displayPhotos() {
+  totalImages = photosArray.length;
+
   photosArray.forEach((photo) => {
     const item = document.createElement("a");
     item.setAttribute("href", photo.links.html);
@@ -18,6 +31,9 @@ function displayPhotos() {
     img.setAttribute("alt", photos.alt_description);
     img.setAttribute("title", photo.alt_description);
   });
+  img.addEventListener("load", imageLoaded);
+  item.appendChild(img);
+  imageContainer.appendChild(item);
 }
 
 async function getPhotos() {
@@ -33,6 +49,7 @@ window.addEventListener("scroll", () => {
     window.innerHeight + window.scrollY >=
     document.body.offsetHeight - 1000
   ) {
+    getPhotos();
   }
 });
 
